db164651a951808aa02bdb3f260a219a8f069feb
anasm23 <muhammadanas112@gmail.com>
2021-12-15 16:10:19 -0500

    styling changes

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -2,9 +2,11 @@ import React from "react";
 
 // Exporting the Container, Row, and Col components from this file
 
+// previous container 
+// className={`container${fluid ? "-fluid" : ""}`}
 // This Container component allows us to use a bootstrap container without worrying about class names
 export function Container({ fluid, children }) {
-  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+  return <div className={`container-fluid`}>{children}</div>;
 }
 
 // This Row component lets us use a bootstrap row without having to think about class names
